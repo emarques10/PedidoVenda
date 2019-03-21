3c004d1194202c1c64ad1c3c0335f39d0f51f3b0
Ewerton Marques <marquescpv@gmail.com>
2019-03-21 09:22:35 -0300

    Tratando exceções

---
+++ src/main/java/com/emarques/pedidovenda/controller/CadastroProdutoBean.java
@@ -0,0 +1,12 @@
+package com.emarques.pedidovenda.controller;
+
+import javax.faces.bean.RequestScoped;
+
+@RequestScoped
+public class CadastroProdutoBean {
+	
+	public void salvar() {
+		throw new RuntimeException("Teste de exceção");
+	}
+
+}
